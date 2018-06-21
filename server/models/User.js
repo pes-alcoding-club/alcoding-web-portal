06eea3d28b7e4a74d62cba35edbda862b606d29d
Aniket Kaulavkar <34034261+aniketnk@users.noreply.github.com>
2018-06-21 20:32:32 +0530

    Add timestamp in UserSchema (#17)
    CreatedAt and UpdatedAt

--- server/models/User.js
+++ server/models/User.js
@@ -28,6 +28,16 @@ const UserSchema = new mongoose.Schema({
     // required: true,
     default: ""
   },
+  timestamp: {
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now()
+    }
+  },
   isDeleted: {
     type: Boolean,
     default: false
