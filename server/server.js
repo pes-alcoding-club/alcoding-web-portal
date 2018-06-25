414ce1fe86409249e12b70b5b22b1988e7bfa39e
Parth Shah <parthvshah@gmail.com>
2018-06-25 23:50:24 +0530

    Added https at 8443

--- server/server.js
+++ server/server.js
@@ -7,8 +7,13 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
+var https = require('https');
+
 const config = require('../config/config');
 const webpackConfig = require('../webpack.config');
+
+var privateKey = fs.readFileSync('server/sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('server/sslcert/server.crt', 'utf8');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const port  = process.env.PORT || 8080;
@@ -60,12 +65,16 @@ if (isDev) {
   });
 }
 
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8443);
+
 app.listen(port, '0.0.0.0', (err) => {
   if (err) {
     console.log(err);
   }
 
-  console.info('>>> 🌎 Open http://0.0.0.0:%s/ in your browser.', port);
+  console.info('>>> ?? Open http://0.0.0.0:%s/ in your browser.', port);
 });
 
 module.exports = app;
