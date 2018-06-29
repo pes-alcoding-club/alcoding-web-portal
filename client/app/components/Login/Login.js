996c717e75a37b3eaf3f5494c1687a9419a958c4
Rishi Ravikumar <rishiravi.k98@gmail.com>
2018-06-29 17:28:50 +0300

    Bug Fixes

--- client/app/components/Login/Login.js
+++ client/app/components/Login/Login.js
@@ -94,7 +94,12 @@ class Login extends Component {
   }
 
   render() {
-    
+    const {
+      signInEmail,
+      signInPassword,
+      showWarning
+    } = this.state;
+
     return (
 
       <div className="container-fluid">
@@ -126,7 +131,7 @@ class Login extends Component {
             <div className="form-group">
               <button type="submit" className="btn btn-primary btn-block" onClick={this.onSignIn}>Log in</button>
             </div>
-            {this.showWarning ? <div className='text-warning'> Invalid sign in, please try again </div> : null}
+            {showWarning ? <div className='text-warning'> Invalid sign in, please try again </div> : null}
           </form>
           
         </div>
