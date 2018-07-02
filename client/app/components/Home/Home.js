d6c357d115eefc6c580ee8f939badb988f0412cb
Parth Shah <parthvshah@gmail.com>
2018-07-02 22:35:25 +0530

    Integrated login with landing page (#37)

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -8,10 +8,33 @@ class Home extends Component {
 
 
   render() {
+    const staticText = {
+      aboutUs: "The Alcoding Club is part of the Computer Science & Engineering Department of PES University. It was formed in 2014 under the supervision of Prof. Channa Bankapur.",
+      latestNews: "2 July 2018: The meeting to kick off the club was successfully conducted in B-Block, G201, the headquaters.",
+      announcements: "Important: All further meetings stand cancelled until further intimation."
+    }
+
     return (
-      <>
-        
-      </>
+        <div className="card-deck">
+          <div className="card bg-light">
+            <div className="card-body text-center">
+            <h4 class="card-title">About Us</h4>
+            <p class="card-text">{staticText.aboutUs}</p>
+            </div>
+          </div>
+          <div className="card bg-light">
+            <div className="card-body text-center">
+            <h4 class="card-title">Latest News</h4>
+            <p class="card-text">{staticText.latestNews}</p>
+            </div>
+          </div>
+          <div className="card bg-light">
+            <div className="card-body text-center">
+            <h4 class="card-title">Announcements</h4>
+            <p class="card-text">{staticText.announcements}</p>
+            </div>
+          </div>
+        </div>
     );
   }
 }
