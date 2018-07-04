5741e4d26875b63ff208415718cd833c50c0c1d0
Aniket Kaulavkar <aniket.kaulavkar@gmail.com>
2018-07-04 21:37:04 +0530

    Fix Courses from old commit

--- client/app/components/Pages/Courses/Courses.js
+++ client/app/components/Pages/Courses/Courses.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
-export default class Courses extends Component {
-  render() {
-    return (
-      <div>
-        Courses Page
-      </div>
-    )
+class Courses extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+    // functions
   }
+
+  componentDidMount(){
+    var token = localStorage.getItem('token');
+    var userID = localStorage.getItem('user_id');
+    if(!token || !userID){
+      console.log("Not logged in.");
+      <Redirect to="/" />
+    }
+
+    var apiPath = '/api/'
+  }
+
 }
+
+export default Courses;
