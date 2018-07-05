62acc8d52ff808b1840b022fdb2cc543695bc0cd
Aditya Vinod Kumar <ads.vinodk@gmail.com>
2018-07-06 00:09:09 +0530

    Implemented line by line validation and call

--- client/app/components/Admin/SignupForm.js
+++ client/app/components/Admin/SignupForm.js
@@ -7,37 +7,33 @@ class SignupForm extends Component {
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.fileInput = React.createRef();
 	}
-	
+
 	handleSubmit(event) {
 		event.preventDefault();
 		var fileObj = this.fileInput.current.files[0];
 		var token = 'Bearer ' + localStorage.getItem('token');
-		
+
 		var configSignup = {
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded',
 				'Authorization': token
 			}
 		};
-		var configUpload = {
-			headers: {
-				'Content-Type': 'multipart/form-data',
-				'Authorization': token
-			}
-		};
-		var all_users=[];
+		var signUpUrl = '/api/admin/signup';
+		// var all_users=[];
 		if (fileObj) {
 			var reader = new FileReader();
 			reader.onload = function (file) {
-			var data= file.target.result.split('\n');
-			data.splice(-1, 1); //splice used to remove one last empty element that gets added due to the csv formatting
-			const NO_OF_MANDATORY_FIELDS = 4; //MODIFY ACCORDING TO NUMBER OF FIELDS REQUIRED FOR SIGNUP
-			var row, valid_count=0;
-				for (row in data) {
-				var invalid=0;
-				var attributes = data[row].split(',');
-					for(var count=0;count<NO_OF_MANDATORY_FIELDS;count++){
-						if(attributes[count]==""){invalid=1;}
+				var data = file.target.result.split('\n');
+				data.splice(-1, 1); //splice used to remove one last empty element that gets added due to the csv formatting
+				const NO_OF_MANDATORY_FIELDS = 4; //MODIFY ACCORDING TO NUMBER OF FIELDS REQUIRED FOR SIGNUP
+				var row, valid_count = 0;
+				for (var row_count = 0; row_count < data.length; row_count++) {
+					row = data[row_count];
+					var invalid = 0;
+					var attributes = row.split(',');
+					for (var count = 0; count < NO_OF_MANDATORY_FIELDS; count++) {
+						if (attributes[count] == "") { invalid = 1; }
 					};//to check cases where there are blank fields for each user
 					if (!invalid) {
 						valid_count++;
@@ -46,58 +42,44 @@ class SignupForm extends Component {
 						body += '&email=' + attributes[1];
 						body += '&password=' + attributes[2];
 						body += '&usn=' + attributes[3];
-						all_users.push(body);//append valid users to a list all_users
+						// all_users.push(body);//append valid users to a list all_users
+						axios.post(signUpUrl, body, configSignup)
+							.then(res => {
+								console.log(res);
+								console.log(res.data);
+							})
+							.catch(err =>
+								console.log(err)
+							)
+					}
+					else {
+						console.log("Line " + row_count + "is invalid");
 					}
 				}
-				var total_rows=(++row);
-				if(total_rows==valid_count){
-					//if valid rows and total rows are same
-					//upload the file and signup all users
-					
-					var uploadUrl = '/api/admin/upload'
-					var formData = new FormData();
-					formData.append("inputFile", fileObj);
-					axios.post(uploadUrl, formData, configUpload)
-					.then(res => {
-						console.log(res);
-						console.log(res.data);
-					})
-
-					var signUpUrl = '/api/admin/signup';
-					all_users.forEach(function(body) {
-    					axios.post(signUpUrl, body, configSignup)
-						.then(res => {
-							console.log(res);
-							console.log(res.data);
-						})
-						.catch(err =>
-							console.log(err)
-						)
-					});	
-
+				var total_rows = (++row);
+				if (total_rows == valid_count) {
+					console.log("All users in the CSV file are valid ")
 				}
-				else{console.log("all users in csv files are not valid!");}
-
+				else { console.log("all users in csv files are not valid!"); }
 			}
 			reader.readAsText(fileObj, "UTF-8");
-			
 		}
-		else{console.log('UPLOAD FILE PLEASE!');}
+		else { console.log('Please Upload a file!'); }
 	}
 
 	render() {
 		return (
 			<div className='container-fluid'>
 				<h3>Upload Student Details :</h3>
-					<form id="formObject">
+				<form id="formObject">
 					<span>Please upload a .csv file</span>
 					<input type="file" className="btn btn-default form-control" ref={this.fileInput} />
 					<br />
 					<button type="submit" className="btn btn-dark form-control w-25" onClick={this.handleSubmit}>Submit</button>
-					</form>
+				</form>
 			</div>
-			);
-		}
+		);
 	}
+}
 
-	export default SignupForm;
+export default SignupForm;
