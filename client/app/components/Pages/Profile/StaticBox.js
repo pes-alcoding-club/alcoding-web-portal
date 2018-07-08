0e787536f6c9248857331f4643933652e1209bdf
Rishi Ravikumar <rishiravi.k98@gmail.com>
2018-07-08 10:39:24 +0300

    Profile Page

--- client/app/components/Pages/Profile/StaticBox.js
+++ client/app/components/Pages/Profile/StaticBox.js
@@ -7,8 +7,8 @@ class StaticBox extends React.Component {
     render() {
         return (
             <form className="form-inline">
-                <div className="form-group mb-2 mx-2">
-                    <label htmlFor="staticTxt" className="sr-only">{this.props.field}</label>
+                <div className="form-group mb-2 mx-2 font-weight-bold">
+                    <label htmlFor="staticTxt" className="sr-only ">{this.props.field}</label>
                     <input type="text" readOnly className="form-control-plaintext" id="staticTxt" value={this.props.field} />
                 </div>
                 
