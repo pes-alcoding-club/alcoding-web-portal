615a5fc7d5a161b7e1d1c42136b245d6046df1e9
Aniket Kaulavkar <fastbum@Matebook-X.localdomain>
2018-07-09 09:38:28 +0530

    Added back config.example.js

---
+++ config/config.example.js
@@ -0,0 +1,6 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb://username:password@url:port/db',
+  db_dev: 'mongodb://url:port/db',
+};
