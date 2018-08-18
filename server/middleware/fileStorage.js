ed326b6431114d735ee71ccffd50ae118d96cc7d
Aditya Vinod Kumar <32671846+adityavinodk@users.noreply.github.com>
2018-08-18 17:34:03 +0530

    Remove assignmentCheck and fileUpload

--- server/middleware/fileStorage.js
+++ server/middleware/fileStorage.js
@@ -1,12 +1,8 @@
 var multer = require('multer');
 const User = require('../models/User');
 const File = require('../models/Files');
-var Course = require('../models/assignments/Course');
-var Assignment = require('../models/assignments/Assignment');
-// var connect = require('connect');
 var fs = require("fs");
 var path = require('path');
-var keyName = "inputFile" //Change according to your key name for file
 
 var diskStorage = function (dir) {
     var storage = multer.diskStorage({
@@ -80,80 +76,6 @@ var fileUpload = function (req, res, next) {
     next();
 }
 
-var assignmentCheck = function (req, res, next) {
-    if(!req.params.userID){
-        return res.status(400).send({
-            success: false,
-            message: "Error: userID not in parameters. Please try again."
-        });
-    }
-    if (!req.file) {
-        return res.status(400).send({
-            success: false,
-            message: "Error: File not recieved"
-        });
-    }
-    if (!req.params.assignmentID) {
-        return res.status(400).send({
-            success: false,
-            message: "Error: Enter assignment ID"
-        });
-    }
-    Course.find({
-        students: req.params.userID,
-        assignments: req.params.assignmentID,
-    }, function(err, courses){
-        if(err){
-            // console.log("hello");
-            return res.status(500).send({
-                success: false,
-                message: "Error: server error"
-            });
-        }
-        if(courses.length==0){
-            return res.status().send({
-                success:false,
-                message:"Error: Given user hasn't signed up for this course"
-            });
-        }
-        // console.log(courses[0]);
-        Assignment.find({
-            _id: req.params.assignmentID,
-            isDeleted: false,
-            "submissions": {user: req.params.userID}
-        }, function(err, assignment){
-            if(err){
-                return res.status(500).send({
-                    success: false,
-                    message: "Error: server error"
-                });
-            }
-            if(assignment){
-                return res.status(400).send({
-                    success: false,
-                    message: "Error: The student has already uploaded the assignment."
-                })
-            }
-        });
-    });
-    next();
-}
-
-// var fileDB = function(middleware) {
-//     if (!middleware.length) {
-//         return function(_req, _res, next) { next(); };
-//     }
-//     var head = middleware[0];
-//     var tail = middleware.slice(1);
-
-//     return function(req, res, next) {
-//         head(req, res, function(err) {
-//             if (err) return next(err);
-//             compose(tail)(req, res, next);
-//         });
-//     };
-// }
-
 var downloadFile = function(dir) {
     return function(req,res,next){
         File.find({
@@ -185,8 +107,6 @@ var downloadFile = function(dir) {
     }
 }
 
-//TODO: Make file downloadable
 //TODO: Delete file endpoint
 
-module.exports = { diskStorage, fileUpload, assignmentCheck, downloadFile };
-// module.exports = { fileDB, assignmentCheck, downloadFile };
+module.exports = { diskStorage, fileUpload, downloadFile };
