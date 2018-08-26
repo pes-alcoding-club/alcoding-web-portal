b5504d7dba9f71cbd1ac51e13b24d69be8d8c8c6
Parth Shah <parthvshah@gmail.com>
2018-08-26 23:34:19 +0530

    Feature/contests (#83)
    * Contests Schema (#16)
    
    * Contests schema
    
    * Updated Contests Schema
    
    * Update accountManagement.js (#12)
    
    Added HTTP Status Codes
    
    * Add timestamp in UserSchema (#17)
    
    CreatedAt and UpdatedAt
    
    * getDetails Endpoint
    
    * Contests Schema (#16)
    
    * Contests schema
    
    * Updated Contests Schema
    
    * Update accountManagement.js (#12)
    
    Added HTTP Status Codes
    
    * Backend contests and Master changes (#27)
    
    * Fix DEPRECATION warning and auto fix dependencies.
    
    Removed the deprecation warning which was seen when the server was started.
    Automatically updated dependencies.
    
    * JWT implemented for logout and getDetails (#21)
    
    * Update login API to support JWT
    
    UserSession.js becomes useless as all data can be stored in the token.
    
    * Added middleware to verify JWT
    
    Added a middle to verify a token.
    Update logout API to support new JWT.
    Still have to admin condition.
    
    * Update getDetails to suport JWT
    
    * Minor fixes after testing
    
    * Modifying endpoints
    
    Pass user_id as a parameter and not a query
    
    * Improved security of jwt
    
    Re-added UserSession table to keep track of logged out tokens.
    
    * Bug fixes
    
    * Optimize verifyToken function
    
    Optimized by removed isLoggedOut field. The token is deleted from db once user logs out.
    
    * Admin only signUp done
    
    Please see TODO, wip.
    
    * Added https at 8443
    
    * Fixed verifyUser.
    
    * POST routes, Contests schema, Server private key path set
    
    * Added GET Requests and Modifications
    
    * API changes and Schema modifications
    
    * Removal of redundant endpoint
    
    * Bug fixes in Schema and Secret Key fix
    
    * Config Server Key
    
    * Fixed req.user Error
    
    * Bug fixes
    
    * Contests rank list

---
+++ server/routes/api/contests.js
@@ -0,0 +1,127 @@
+const User = require('../../models/User');
+const File = require('../../models/Files');
+var requireRole = require('../../middleware/Token').requireRole;
+// var fileDB = require('../../middleware/fileStorage').fileDB;
+var diskStorage = require('../../middleware/fileStorage').diskStorage;
+var fileUpload = require('../../middleware/fileStorage').fileUpload;
+var retrieveFile = require('../../middleware/fileStorage').retrieveFile;
+var fs = require("fs");
+var dir = process.cwd() + '/../temp';
+var keyName = "inputFile";
+
+module.exports = (app) => {
+  app.get('/api/contests/globalRankList', function (req, res) {
+    User.find({
+      isDeleted: false
+    }, (err, users) => {
+      if (err) {
+        return res.status(500).send({
+          success: false,
+          message: "Error: Server error."
+        });
+      }
+      if (!users) {
+        return res.status(404).send({
+          success: false,
+          message: 'No users'
+        });
+      }
+      var userContenderDetails = [];
+      for (var user of users) {
+        var name = user.name.firstName + " " + user.name.lastName;
+        pushObject = Object.assign({ usn: user.usn, name }, user.contender.toObject());
+        pushObject.rating = Math.round(pushObject.rating);
+        pushObject.best = Math.round(pushObject.best);
+        userContenderDetails.push(pushObject);
+      }
+      return res.status(200).send({
+        success: true,
+        message: "globalRankList retrieved.",
+        globalRankList: { userContenderDetails }
+      });
+
+    })
+  })
+
+  app.post('/api/contests/updateContenders', requireRole("admin"), function (req, res) {
+
+    var usn = req.body.usn;
+    var name = req.body.name;
+    var email = req.body.email;
+    var codejam = req.body.codejam;
+    var rating = req.body.rating;
+    var volatility = req.body.volatility;
+    var timesPlayed = req.body.timesPlayed;
+    var lastFive = req.body.lastFive;
+    var best = req.body.best;
+    var hackerearth = req.body.hackerearth;
+
+    if (!usn) {
+      return res.status(400).send({
+        success: false,
+        message: 'Error: First name cannot be blank.'
+      });
+    }
+
+    // Process data
+    usn = ('' + usn).toUpperCase().trim();
+
+    // Deduplication flow
+    User.find({
+      usn: usn
+    }, (err, previousUsers) => {
+      if (err) {
+        return res.status(500).send({
+          success: false,
+          message: 'Error: Server find error'
+        });
+      } else if (previousUsers.length > 0) { //Update
+        previousUsers[0].contender.handles["codejam"] = codejam;
+        previousUsers[0].contender.handles["hackerearth"] = hackerearth;
+        previousUsers[0].contender.rating = rating;
+        previousUsers[0].contender.volatility = volatility;
+        previousUsers[0].contender.timesPlayed = timesPlayed;
+        previousUsers[0].contender.lastFive = lastFive;
+        previousUsers[0].contender.best = best;
+        return res.status(200).send({
+          success: true,
+          message: 'Updated Contender'
+        });
+      }
+      else {
+        //New user
+        const newUser = new User();
+
+        newUser.usn = usn;
+        if (name) { newUser.name.firstName = name.split(" ")[0]; newUser.name.lastName = name.split(" ")[1]; }
+        if (email) { newUser.basicInfo.email = email; }
+        newUser.password = newUser.generateHash(usn);
+        newUser.role = "student";
+
+        newUser.contender.handles["codejam"] = codejam;
+        newUser.contender.handles["hackerearth"] = hackerearth;
+        newUser.contender.rating = rating;
+        newUser.contender.volatility = volatility;
+        newUser.contender.timesPlayed = timesPlayed;
+        newUser.contender.lastFive = lastFive;
+        newUser.contender.best = best;
+
+        newUser.save((err, user) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).send({
+              success: false,
+              message: 'Server error'
+            });
+          }
+          console.log(newUser._id + " added to DB.")
+          return res.status(200).send({
+            success: true,
+            message: 'Signed Up'
+          });
+        });
+      }
+
+    });
+  });
+}
