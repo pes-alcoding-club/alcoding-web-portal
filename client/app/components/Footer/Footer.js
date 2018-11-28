54487dd33bd1d306158363fcd4c9ce1aab34d39f
Aniket Kaulavkar <aniket.kaulavkar@gmail.com>
2018-11-29 01:11:52 +0530

    Adding link to contribute page

--- client/app/components/Footer/Footer.js
+++ client/app/components/Footer/Footer.js
@@ -5,6 +5,8 @@ const Footer = () => (
     <hr />
     <div className="row text-center">
       <div className="col-12">
+        <div className="lead">We'd love your help! <a href='/contribute'>Contribute</a> to this project on Github.</div>
+        {/* {'\n'} */}
         <p>Copyright © The Alcoding Club, CS&E, PES University, Bengaluru, India.</p>
       </div>
     </div>
