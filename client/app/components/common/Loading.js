b083cd58a478acae8a9e413b3b8546d2f1a632cc
Aniket Kaulavkar <aniket.kaulavkar@gmail.com>
2019-06-05 15:40:57 +0530

    Merging development features

--- client/app/components/common/Loading.js
+++ client/app/components/common/Loading.js
@@ -8,16 +8,17 @@ class Loading extends React.Component {
     render() {
         return (
             <div className='d-flex justify-content-center'>
-                <div className="jumbotron py-1  px-4 bg-light">
+                {/* <div className="jumbotron py-1  px-4 bg-light"> */}
                     <div className="row text-center">
                         <div className="col-12">
                             <ReactLoading 
                                 type={this.props.type ? this.props.type : "bubbles"} 
-                                color={this.props.color ? this.props.color : "#25799f"} 
+                            // color={this.props.color ? this.props.color : "#18bc9c"} 
+                                color={this.props.color ? this.props.color : "#212529"} 
                                 />
                             {/* Loading.. */}
                         </div>
-                    </div>
+                    {/* </div> */}
                 </div>
             </div>
         );
