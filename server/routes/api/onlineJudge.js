e31e0bc99b2948780f0eb898e12f50d46dc876ef
Ishar-Menon <ishar1999messi@gmail.com>
2019-07-17 12:21:27 +0530

    Added documentation for Online judge API and Updated  README.md

--- server/routes/api/onlineJudge.js
+++ server/routes/api/onlineJudge.js
@@ -1,5 +1,24 @@
 const getOutput = require("../../utils/dockerAPI");
-
+// A json object has to passed with request body to this endpoint,  the json has the format
+// {
+//   "data": {
+//       "language": "cpp",
+//       "testCases": [
+//           "1",
+//           "2",
+//           "3"
+//       ],
+//       "timeout": "1s",
+//       "files": [
+//           {
+//               "name": "foo.cpp",
+//               "content": "#include <stdio.h>\nint main(){ //Test comment. \nint w; scanf(\"%d\", &w); printf(\"Number: %d\", w);   return 0;}"
+//           }
+//       ]
+//   }
+// }
+// It calls the getOutput function, passing the data json  as the parameter, and return the output json of the getOutput 
+// function.
 module.exports = app => {
   app.post("/api/onlineJudge/submit", async (req, res) => {
     const data = req.body.data || new Object({});
